3a20a755957236f57fa7c936d61014f7b1ae5c57
Michel Hense <kjs.kyff@gmail.com>
2023-10-28 10:54:18 +0200

    new

--- html_projekte/script.js
+++
@@ -1,8 +0,0 @@
-function checkPassword() {
-	var password = document.getElementById("password").value;
-	if (password == "zwen") {
-		window.location.href = "Test.html";
-	} else {
-		document.getElementById("message").innerHTML = "Falsches Passwort. Bitte versuchen Sie es erneut.";
-	}
-}
--- rcsv/ukraine.R
+++
@@ -1,4 +0,0 @@
-d <- read.csv("/Users/dianahense/Desktop/rcsv/ukraine3.txt", sep=",")
-d
-boxplot(d$Financial.commitments, d$Humanitarian.commitments, d$Military.commitments, d$Total.bilateral.commitments)
-/Users/dianahense/Desktop
